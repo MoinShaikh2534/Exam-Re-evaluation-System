a0fcd7041a6aad61f9c306f698b6554f030f6750
Neeraj <neerajsurnis@gmail.com>
2025-02-22 13:53:40 +0530

    Modified UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,30 @@ import Home from "./pages/Home";
 import Result from "./pages/Result";
 import ReEvaluation from "./pages/ReEvaluation";
 import Login from "./pages/Login";
+import StudentLogin from "./pages/StudentLogin";
 import Logout from "./pages/Logout";
 
 const App = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} /> {/* Add this line */}
-          <Route path="/result" element={<Result />} />
-          <Route path="/re-evaluation" element={<ReEvaluation />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/faculty/login" element={<Login />} />
+        <Route path="/student/login" element={<StudentLogin />} />
+        <Route
+          path="/*"
+          element={
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/result" element={<Result />} />
+                <Route path="/re-evaluation" element={<ReEvaluation />} />
+                <Route path="/logout" element={<Logout />} />
+              </Routes>
+            </Layout>
+          }
+        />
+      </Routes>
     </Router>
   );
 };
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Profile from "./Profile";
 
 const Layout = ({ children }) => {
+  const location = useLocation(); // Get the current route path
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       {/* Navbar */}
-      <Navbar />
 
-      {/* Profile Section */}
-      <div className="min-h-20 flex flex-col bg-gray-100 mt-10 mx-10">
-        <Profile />
-      </div>
+
+      {/* Profile Section (Exclude on /login page) */}
+      {location.pathname !== "/login" && (
+        <div className="min-h-20 flex flex-col bg-gray-100 mt-10 mx-10">
+          <Navbar />
+          <Profile />
+        </div>
+      )}
 
       {/* Main Content (Page Content) */}
       <main className="flex-grow p-8 max-w-6xl mx-auto mt-10">{children}</main>
