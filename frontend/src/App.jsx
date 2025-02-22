e0d818d1eeefac7d09aaa3fbeba99d34a18d3781
Moin Shaikh <22ucs119@dkte.ac.in>
2025-02-22 23:33:20 +0530

    cashier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import StudentDashboard from "./pages/StudentDashboard";
-import Casher from "./pages/Casher";
+import Casher from "./pages/Cashier";
 import Result from "./pages/Result";
 import ReEvaluation from "./pages/ReEvaluation";
 import StudentLogin from "./pages/StudentLogin";
@@ -21,7 +21,7 @@ const App = () => {
           <Route path="/home" element={<StudentDashboard />} />
           <Route path="/result" element={<Result />} />
           <Route path="/re-evaluation" element={<ReEvaluation />} />
-          <Route path="/casher" element={<Casher />} />
+          <Route path="/cashier" element={<Casher />} />
           <Route path="/logout" element={<Logout />} />
         </Route>
       </Routes>
